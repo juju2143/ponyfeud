50b7f8073f36ea4d525635da9de979e879d97ca9
juju2143 <juju2143@gmail.com>
2016-11-06 15:25:46 -0500

    Production config.js

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
 var surveys = {
-  "test": [["lyra",53],["bon bon",12],["surprise",10],["moondancer",4]],
-  "test2": [["time",21],["test",15],["great Googly Moogly",11],["a",16],["15",9],["texty.texty",2],["goo",5],["foo",1]],
+  "Question 1": [["Cupcakes",44],["My Little Dashie",24],["Rainbow Factory",15],["Fallout Equestria",10],["Past Sins",6]],
+  "Question 2": [["Flash",35],["Diamond T",23],["Spoiled Rich",12],["Starlight",11],["Trixie",9],["Applejack",6],["Snips",4]],
+  "Question 3": [["Derptor",46],["Lyrabon",35],["Rainbowsorin",13],["Spike & Rarity",5],["Twiflash",1]],
+  "Question 4": [["Derpy",32],["BonBon",25],["Lyra",17],["Vinyl",10],["Octavia",9],["Doctor",4],["Big Mac",2],["Roseluck",1]],
+  "Question 5": [["Cake",57],["Pudding",23],["Fountain",18],["Cookie",2]],
+  "Question 6": [["Sunset Shimmer",46],["Celestia",25],["Cheerilee",17],["Coco",8],["Granny Smith",4]],
+  "Question 7": [["Gilda",32],["Amber",28],["Discord",15],["Thorax",10],["Zecora",6],["Steven Magnet",3],["Tom",4]],
+  "Question 8": [["Dat Boi",30],["Pepe",18],["All",16],["Harambe",13],["Spongegar",11],["Trollface",7],["Dab",5]],
+  "Question 9": [["Friends",8],["Sweetie Belle",7],["Fashion",15],["Jewels/Gems",20]],
 };
 var colorSchemes = [["Purple/Pink","twicastle.css"],["White/Blue","crystalcastle.css"]];
